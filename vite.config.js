2fbd7ad982b6aadad18784c3842582fe06a25257
VictorV <vvega28@outlook.com>
2026-04-11 17:52:54 -0600

    fixinf vite

--- vite.config.js
+++ vite.config.js
@@ -13,15 +13,6 @@ export default defineConfig({
           __dirname,
           "src/pages/team.html",
         ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(
-          __dirname,
-          "src/product_pages/northface-alpine-3.html",
-        ),
-        product4: resolve(
-          __dirname,
-          "src/product_pages/northface-talus-4.html",
-        ),
       },
     },
   },
