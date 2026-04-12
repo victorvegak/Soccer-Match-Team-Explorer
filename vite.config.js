53c18f522af917db7cf4473d08f3afd447886892
VictorV <vvega28@outlook.com>
2026-04-11 18:24:56 -0600

    fixing vite

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,17 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
-        product1: resolve(
-          __dirname,
-          "src/pages/team.html",
-        ),
+        product1: resolve(__dirname, "src/pages/team.html"),
       },
     },
   },
